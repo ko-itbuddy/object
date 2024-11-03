e3cb3fee0b8b572f88dc7337a256b832aede0120
나평근 <skvudrms54@gmail.com>
2024-11-04 00:54:22 +0900

    object-2 CH01 TO BE 소극장 티켓 이벤트 입장 구현 개선

---
+++ src/main/java/ch01/tobe/Audience.java
@@ -0,0 +1,18 @@
+package ch01.tobe;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+
+
+    public Long buy(Ticket ticket){
+        return bag.hold(ticket);
+    }
+}
